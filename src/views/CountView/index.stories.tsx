2469ac16301d280882688274fac04cf26d6e71f6
NiheiKeita <86122925+NiheiKeita@users.noreply.github.com>
2024-02-21 07:45:16 +0900

    自動テスト

--- src/views/CountView/index.stories.tsx
+++ src/views/CountView/index.stories.tsx
@@ -1,5 +1,7 @@
-import { Meta, StoryObj } from '@storybook/react'
-import { CountView } from '.'
+import { expect } from '@storybook/jest';
+import { Meta, StoryObj } from '@storybook/react';
+import { screen, userEvent } from '@storybook/testing-library';
+import { CountView } from '.';
 
 const meta: Meta<typeof CountView> = {
   title: 'views/CountView',
@@ -12,8 +14,16 @@ type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
   args: {
+    
   },
   render: function Render() {
     return <CountView />
   },
+}
+Default.play = async ({ args }) => {
+  const button = await screen.findByTestId('button');
+  expect(button).toBeInTheDocument();
+  expect(screen.queryByTestId('countText')?.innerHTML).toBe("0");
+  await userEvent.click(button);
+  expect(screen.queryByTestId('countText')?.innerHTML).toBe("1");
 }
--- src/views/CountView/index.tsx
+++ src/views/CountView/index.tsx
@@ -5,8 +5,8 @@ export const CountView = React.memo(() => {
   const { count, addCount } = useCountView();
   return (
     <div>
-      <p>{count}</p>
-      <button className="bg-red-500 px-2 py-4 hover:bg-red-200" onClick={addCount}>
+      <p data-testid="countText">{count}</p>
+      <button data-testid="button" className="bg-red-500 px-2 py-4 hover:bg-red-200" onClick={addCount}>
         カウントアップ
       </button>
     </div>
