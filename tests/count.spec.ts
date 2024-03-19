f5c3ae5e8cd9ec1b072df1635f93e266385ade5d
NiheiKeita <86122925+NiheiKeita@users.noreply.github.com>
2024-03-19 23:10:27 +0900

    修正

--- tests/count.spec.ts
+++ tests/count.spec.ts
@@ -1,6 +1,5 @@
 import { test } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  // await page.goto('http://localhost:3000/count');
-await page.goto('');
+  await page.goto('http://localhost:3000/count');
 });
