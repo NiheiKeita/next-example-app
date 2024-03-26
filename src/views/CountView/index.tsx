325f8e2bbfb4c6ae04a8844f7351fc5ab7878efd
NiheiKeita <86122925+NiheiKeita@users.noreply.github.com>
2024-03-26 09:17:13 +0900

    修正

--- src/views/CountView/index.tsx
+++ src/views/CountView/index.tsx
@@ -6,7 +6,7 @@ export const CountView = React.memo(() => {
   return (
     <div>
       <p data-testid="countText">{count}</p>
-      <button data-testid="button" className="bg-red-500 px-2 py-4 hover:bg-red-200" onClick={addCount}>
+      <button data-testid="button" className="bg-red-500 px-2 py-4 hover:bg-red-300" onClick={addCount}>
         カウントアップ==
       </button>
     </div>
